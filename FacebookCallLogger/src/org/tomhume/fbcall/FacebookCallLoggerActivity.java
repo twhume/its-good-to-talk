9936d13ecfa5d572d9c99f3efd5ee97452f09a7e
Tom Hume <twhume@gmail.com>
2012-06-01 21:26:53 +0100

    Looking up name from contacts database now...

--- FacebookCallLogger/src/org/tomhume/fbcall/FacebookCallLoggerActivity.java
+++ FacebookCallLogger/src/org/tomhume/fbcall/FacebookCallLoggerActivity.java
@@ -1,20 +1,35 @@
 package org.tomhume.fbcall;
 
 import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.BaseColumns;
+import android.provider.ContactsContract;
+import android.provider.ContactsContract.Contacts;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
 import com.facebook.android.*;
 import com.facebook.android.Facebook.*;
 
 
-public class FacebookCallLoggerActivity extends Activity {
+public class FacebookCallLoggerActivity extends Activity implements OnClickListener {
 
-    Facebook facebook = new Facebook("429957320362642");
+	private static final String TAG = "FacebookCallLoggerActivity";
+    private Facebook facebook = new Facebook("429957320362642");
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        Button button = (Button)findViewById(R.id.test_button);
+        button.setOnClickListener(this);
 
         facebook.authorize(this, new DialogListener() {
             @Override
@@ -30,12 +45,42 @@ public class FacebookCallLoggerActivity extends Activity {
             public void onCancel() {}
         });
     }
-
+    
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
         facebook.authorizeCallback(requestCode, resultCode, data);
     }
+
+	@Override
+	public void onClick(View v) {
+		Log.d(TAG, "CLICK!");
+		String name = getContactNameFromNumber("07929169110");
+//		String profileId = getProfileIdFromFacebook();
+		Log.d(TAG, "name="+name);
+	}
+	
+	public String getContactNameFromNumber(String number) {
+	    Uri uri = Uri.withAppendedPath(ContactsContract.PhoneLookup.CONTENT_FILTER_URI, Uri.encode(number));
+	    String name = "?";
+
+	    ContentResolver contentResolver = getContentResolver();
+	    Cursor contactLookup = contentResolver.query(uri, new String[] {BaseColumns._ID,
+	            ContactsContract.PhoneLookup.DISPLAY_NAME }, null, null, null);
+
+	    try {
+	        if (contactLookup != null && contactLookup.getCount() > 0) {
+	        		contactLookup.moveToNext();
+	        		name = contactLookup.getString(contactLookup.getColumnIndex(ContactsContract.Data.DISPLAY_NAME));
+	        	}
+	            Log.d(TAG, contactLookup.getString(contactLookup.getColumnIndex(BaseColumns._ID)));
+	    } finally {
+	        if (contactLookup != null) {
+	            contactLookup.close();
+	        }
+	    }
+
+	    return name;
+	}
     
 }
