a5c25a076e1cb0f5259554459648c38e7b48ecd8
Tom Hume <twhume@gmail.com>
2012-06-02 02:00:35 +0100

    Compiler warnings fixed.

--- FacebookCallLogger/src/org/tomhume/fbcall/PhoneCallReporter.java
+++ FacebookCallLogger/src/org/tomhume/fbcall/PhoneCallReporter.java
@@ -4,7 +4,6 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.SharedPreferences.Editor;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 
